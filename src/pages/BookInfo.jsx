52e545a9745c9aed0e04b7b6a064b0cc4786e6e9
Sydney Knoelk <sydneykn548@gmail.com>
2022-07-16 00:56:44 +0200

    with '#' will be ignored, and an empty message aborts the commit.

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { useParams } from "react-router";
-import Ratings from "../components/ui/Ratings";
-import Price from "../components/ui/Price";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
-import BestBooks from "../components/ui/BestBooks";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Book from "../components/Book";
+import Price from "../components/ui/Price";
+import Ratings from "../components/ui/Ratings";
 
-const BookInfo = ({ books, addItemToCart }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
   const { id } = useParams();
   const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find(book => book.id === +id)
+  }
 
   return (
     <div id="books__body">
@@ -25,7 +32,7 @@ const BookInfo = ({ books, addItemToCart }) => {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img className="book__selected--img" src={book.url} alt="" />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
                 <h2 className="book__selected--title">{book.title}</h2>
@@ -39,33 +46,45 @@ const BookInfo = ({ books, addItemToCart }) => {
                 <div className="book__summary">
                   <h3 className="book__summary--title">Summary</h3>
                   <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Veniam, repellendus modi odio porro, consequuntur,
-                    asperiores minima sint voluptatem at reiciendis ducimus
-                    neque provident alias iure nihil explicabo nobis id
-                    voluptas.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Porro soluta tempore cum rem ad possimus esse, dicta qui
+                    eos? Asperiores maiores dicta quae impedit harum cupiditate
+                    delectus nisi soluta quos.
                   </p>
                   <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Veniam, repellendus modi odio porro, consequuntur,
-                    asperiores minima sint voluptatem at reiciendis ducimus
-                    neque provident alias iure nihil explicabo nobis id
-                    voluptas.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Porro soluta tempore cum rem ad possimus esse, dicta qui
+                    eos? Asperiores maiores dicta quae impedit harum cupiditate
+                    delectus nisi soluta quos.
                   </p>
                 </div>
-                <button className="btn" onClick={() => addItemToCart(book)}>
-                  Add to Cart
-                </button>
+                {bookExistsOnCart() ? (
+                    <Link to="/cart" className="book__link">
+                  <button className="btn">Checkout</button>
+                    </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to Cart
+                  </button>
+                )}
               </div>
             </div>
           </div>
         </div>
+
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
               <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
-            <BestBooks id={id} />
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
           </div>
         </div>
       </main>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,42 +2,49 @@ import React from "react";
 import { Link } from "react-router-dom";
 import EmptyCart from "../assets/empty_cart.svg";
 
-const Cart = ({ cart, updateCart, removeItem, totals }) => {
+const Cart = ({ cart, changeQuantity, removeItem }) => {
+  const total = () => {
+    let price = 0;
+    cart.forEach((item) => {
+      price += +(item.salePrice || item.originalPrice * item.quantity);
+    });
+    return price;
+  };
+
   return (
     <div id="books__body">
       <main id="books__main">
         <div className="books__container">
           <div className="row">
-            <div className="book__selected--top">
+            <div className="books__selected--top">
               <h2 className="cart__title">Cart</h2>
             </div>
             <div className="cart">
               <div className="cart__header">
                 <span className="cart__book">Book</span>
                 <span className="cart__quantity">Quantity</span>
-                <span className="cart__total">Price</span>
+                <span className="cart__price">Price</span>
               </div>
               <div className="cart__body">
-                {cart.map((item) => {
-                  const itemPrice = item.salePrice || item.originalPrice;
+                {cart.map((book) => {
                   return (
-                    <div className="cart__item" key={item.id}>
+                    <div className="cart__item">
                       <div className="cart__book">
                         <img
+                          src={book.url}
                           className="cart__book--img"
-                          src={item.url}
                           alt=""
                         />
                         <div className="cart__book--info">
                           <span className="cart__book--title">
-                            {item.title}
+                            {book.title}
                           </span>
                           <span className="cart__book--price">
-                            ${itemPrice.toFixed(2)}
+                            ${(book.salePrice || book.originalPrice).toFixed(2)}
                           </span>
                           <button
                             className="cart__book--remove"
-                            onClick={() => removeItem(item)}
+                            onClick={() => removeItem(book)}
                           >
                             Remove
                           </button>
@@ -46,48 +53,53 @@ const Cart = ({ cart, updateCart, removeItem, totals }) => {
                       <div className="cart__quantity">
                         <input
                           type="number"
-                          className="cart__input"
                           min={0}
                           max={99}
-                          value={item.quantity}
+                          className="cart__input"
                           onChange={(event) =>
-                            updateCart(item, event.target.value)
+                            changeQuantity(book, event.target.value)
                           }
+                          value={book.quantity}
                         />
                       </div>
                       <div className="cart__total">
-                        ${(itemPrice * item.quantity).toFixed(2)}
+                        $
+                        {(
+                          (book.salePrice || book.originalPrice) * book.quantity
+                        ).toFixed(2)}
                       </div>
                     </div>
                   );
                 })}
-                {/* {(!cart || !cart.length) && <img src={EmptyCart}/>} */}
-                {(!cart || !cart.length) && (
-                  <div className="cart__empty">
-                    <img className="cart__empty--img" src={EmptyCart} alt="" />
-                    <h2>You don't have any books in your cart!</h2>
-                    <Link to="/books">
-                      <button className="btn">Browse books</button>
-                    </Link>
-                  </div>
-                )}
               </div>
+              {cart.length === 0 && (
+                <div className="cart__empty">
+                  <img src={EmptyCart} alt="" className="cart__empty--img" />
+                  <h2>You don't have any books in your cart!</h2>
+                  <Link to="/books">
+                    <button className="btn">Browse books</button>
+                  </Link>
+                </div>
+              )}
             </div>
-            {cart && cart.length > 0 && (
+            {cart.length > 0 && (
               <div className="total">
                 <div className="total__item total__sub-total">
                   <span>Subtotal</span>
-                  <span>${totals.subtotal.toFixed(2)}</span>
+                  <span>${(total() * 0.9).toFixed(2)}</span>
                 </div>
                 <div className="total__item total__tax">
                   <span>Tax</span>
-                  <span>${totals.tax.toFixed(2)}</span>
+                  <span>${(total() * 0.1).toFixed(2)}</span>
                 </div>
                 <div className="total__item total__price">
                   <span>Total</span>
-                  <span>${totals.total.toFixed(2)}</span>
+                  <span>${total().toFixed(2)}</span>
                 </div>
-                <button className="btn btn__checkout no-cursor" onClick={() => alert(`Haven't got around to doing this :(`)}>
+                <button
+                  className="btn btn__checkout no-cursor"
+                  onClick={() => alert("Have not got around to doing this")}
+                >
                   Proceed to checkout
                 </button>
               </div>
